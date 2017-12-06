c05c0350540cc3dc3b3cf56ddc1bcbebc1741bf4
Bob <bob.yuan@ixdigit.com>
2017-12-06 15:49:33 +0800

    .

--- YSKit/YSKit/AppDelegate.m
+++ YSKit/YSKit/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"测试Tag修改和合并");
     return YES;
 }
 
